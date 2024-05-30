6619deaa4633a2b1add48e0787587ccc84258a97
Kasia <kasia1781@gmail.com>
2024-05-30 18:20:03 +0200

    import showCart function in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,20 @@
+import { useContext } from 'react';
 import logoImg from '../assets/logo.jpg';
 import { useCartContext } from '../store/shopping-cart-context';
 import Button from './UI/Button';
+import UserProgressContext from '../store/UserProgressContext';
 
 export default function Header() {
 	const { items } = useCartContext();
+	const { showCart } = useContext(UserProgressContext);
+
 	const totalCartItem = items.reduce((totalNumberOfItems, item) => {
 		return totalNumberOfItems + item.quantity;
 	}, 0);
+
+	function handleShowCart() {
+		showCart();
+	}
 
 	return (
 		<header id='main-header'>
@@ -15,7 +23,9 @@ export default function Header() {
 				<h1>ReactFood</h1>
 			</div>
 			<p>
-				<Button textOnly>Cart ({totalCartItem})</Button>
+				<Button onClick={handleShowCart} textOnly>
+					Cart ({totalCartItem})
+				</Button>
 			</p>
 		</header>
 	);
