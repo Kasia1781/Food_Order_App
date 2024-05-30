d5ea65c670181f00189b5b624105387096c1b156
Kasia <kasia1781@gmail.com>
2024-05-30 18:13:50 +0200

    creating UserProgressContextProvider function

---
+++ src/store/UserProgressContext.tsx
@@ -0,0 +1,65 @@
+import { createContext, useState, type ReactNode } from 'react';
+//kontekst do otwarcia koszyka lub pokazania kasy
+
+type UserProgressContextType = {
+	progress: 'cart' | 'checkout' | '';
+	showCart: () => void;
+	hideCart: () => void;
+	showCheckout: () => void;
+	hideCheckout: () => void;
+};
+
+const initialState: UserProgressContextType = {
+	progress: '',
+	showCart: () => {},
+	hideCart: () => {},
+	showCheckout: () => {},
+	hideCheckout: () => {},
+};
+
+const UserProgressContext =
+	createContext<UserProgressContextType>(initialState);
+
+type UserProgressProviderProps = {
+	children: ReactNode;
+};
+
+export function UserProgressContextProvider({
+	children,
+}: UserProgressProviderProps) {
+	const [userProgress, setUserProgress] = useState<'cart' | 'checkout' | ''>(
+		''
+	);
+
+	function showCart() {
+		setUserProgress('cart');
+	}
+
+	function hideCart() {
+		setUserProgress('');
+	}
+
+	function showCheckout() {
+		setUserProgress('checkout');
+	}
+
+	function hideCheckout() {
+		setUserProgress('');
+	}
+
+	const userProgressCtx = {
+		progress: userProgress,
+		showCart,
+		hideCart,
+		showCheckout,
+		hideCheckout,
+	};
+
+	return (
+		<UserProgressContext.Provider value={userProgressCtx}>
+			{children}
+		</UserProgressContext.Provider>
+	);
+}
+
+export default UserProgressContext;
